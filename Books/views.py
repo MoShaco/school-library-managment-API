164fae018ff90b0aa3564b2e042e3fcf79d758a4
Mohammed Shaco <mohammedshaco@gmail.com>
2024-11-22 08:44:56 +0200

    Create views for authors, categories, and books endpoints

--- Books/views.py
+++ Books/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Author, Category, Book
+from .serializers import AuthorSerializer, CategorySerializer, BookSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+class AuthorsView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class BooksView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.select_related('category', 'author').all()
+    serializer_class = BookSerializer
